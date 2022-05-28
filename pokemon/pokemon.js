26523e715972da474fcc259c94100525bcfbc788
zuniscool <jjun1824@naver.com>
2022-05-28 10:13:40 +0900

    주석 수정

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -6,19 +6,19 @@ const requestURL = './data/pokemonList.json';
 // 요청을 만들기 위해, XMLHttpRequest 생성자로부터 새로운 request 인스턴스 생성
 const request = new XMLHttpRequest();
 
-// open() 메서드로 새로운 요청 생성
+// open() 메서드로 새로운 요청 생성함
 request.open('GET', requestURL);
 
-// responseType을 json으로 설정
-// XHR로 하여금 서버가 json 데이터를 반환할 것이며, js 객체로 변환될 것이라는 걸 명시
+// responseType을 json으로 설정함
+// XHR로 하여금 서버가 json 데이터를 반환할 것이며, js 객체로 변환될 것이라는 걸 명시함
 request.responseType = 'json';
 
-// send() 메서드로 요청 보내기
+// send() 메서드로 요청 보냄
 request.send();
 
-// request.onload 로 서버의 응답을 기다린 후 함수 실행
+// request.onload 로 서버의 응답을 기다린 후 함수 실행함
 request.onload = function() {
-  // response 프로퍼티를 사용하여, json 데이터에 기반한 js 객체를 변수에 저장
+  // response 프로퍼티를 사용하여, json 데이터에 기반한 js 객체를 변수에 저장함
   const pokemonList = request.response;
 
   const list = document.querySelector('.wrap_list');
@@ -28,27 +28,28 @@ request.onload = function() {
   function showList(txt) {
     list.innerHTML = '';
   
-    // txt가 빈 값이 아니면
+    // txt가 빈 값이 아니면, for문으로 pokemonList 순회함
     if(txt !== '') {
       for (let i = 0; i < pokemonList.length; i++) {
         const li = document.createElement('li');
         const pokemon = pokemonList[i];
   
-        // pokemon.name이 txt를 포함하면
+        // pokemon.name이 txt를 포함하면,
         if(pokemon.name.includes(txt)) {
+          // li에 포켓몬 정보를 담아,
           li.innerHTML = `
             <img src='${pokemon.url}' alt='${pokemon.name}'>
             <br/><span class='txt_name'>${pokemon.name}</span>
           `
   
-          // list에 자식 요소(li) 추가
+          // list(ul.wrap_list)에 자식 요소(li) 추가
           list.appendChild(li);
         }
       }
-    } else alert('포켓몬 이름을 입력하세요.'); // pokemon.name이 txt를 포함하지 않으면
+    } else alert('포켓몬 이름을 입력하세요.'); // pokemon.name이 txt를 포함하지 않으면 경고창 띄움
   }
   
-  // searchBtn이 click 되면
+  // searchBtn(검색)으로 포켓몬 검색하기
   searchBtn.addEventListener('click', (e) => {
     e.preventDefault(); // 기본 이벤트 방지
     const txt = searchInput.value;
